7030722d626c2d5483a1b97ceefcc9032f0d3344
Jan Lüthje <37576268+LLoydcc@users.noreply.github.com>
2022-11-09 16:25:50 +0100

    add faculty & fibonacci to Akkumulator

--- src/Akkumulator.hs
+++ src/Akkumulator.hs
@@ -1,5 +1,24 @@
 module Akkumulator() where 
 
+-- Durch die Akkumulator-Technik kann, durch speichern des Zwischenergebnis im Akkumulator, ein Iterativer Prozess abgebildet werden. 
+
+-- Berechnet die Fakultät
+faculty :: Integer -> Integer
+faculty n = faculty' n 0
+    where 
+        faculty' 0 ergebnis = ergebnis
+        faculty' n ergebnis = faculty' (n - 1) (n^2 + ergebnis)
+
+
+-- Berechnet die Fakultät
+fibonacci :: Integer -> Integer
+fibonacci n = fibonacci' n 0 1
+    where 
+    fibonacci' 0 akk1 akk2 = akk1
+    fibonacci' 1 akk1 akk2 = akk2
+    fibonacci' n akk1 akk2 = fibonacci' (n - 1) akk2 (akk1 + akk2) 
+    
+
 -- Nimmt eine Liste von Noten [(Matrikelnummer, Note)] entgegen & berechnet den Durchschnitt aller Noten.
 durchschnitt :: [(Integer, Double)] -> Double
 durchschnitt notenliste = durchschnitt' notenliste 0 0
